0ba66f84c38010ac85dd9edab7c9f6fba9b658f1
LinXunFeng <linxunfeng@yeah.net>
2026-06-01 16:19:53 +0800

    fix(ios): guard stopRunning when scanner session is stopped

--- ios/qr_code_scanner_plus/Sources/qr_code_scanner_plus/NativeBarcodeScanner.swift
+++ ios/qr_code_scanner_plus/Sources/qr_code_scanner_plus/NativeBarcodeScanner.swift
@@ -134,7 +134,9 @@ class NativeBarcodeScanner: NSObject, AVCaptureMetadataOutputObjectsDelegate {
 
         let sess = session
         sessionQueue.async {
-            sess?.stopRunning()
+            if sess?.isRunning == true {
+                sess?.stopRunning()
+            }
         }
 
         DispatchQueue.main.async {
